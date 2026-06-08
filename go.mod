7e20761ad3874b390fd81667a736c7a9b37f2dd3
Anyanwu-Carl <anyanwuocee@gmail.com>
2026-06-08 14:51:33 +0100

    First commit

---
+++ go.mod
@@ -0,0 +1,16 @@
+module main.go
+
+go 1.25.5
+
+require (
+	github.com/clipperhouse/displaywidth v0.11.0 // indirect
+	github.com/clipperhouse/uax29/v2 v2.7.0 // indirect
+	github.com/gdamore/encoding v1.0.1 // indirect
+	github.com/gdamore/tcell/v2 v2.13.10 // indirect
+	github.com/gdamore/tcell/v3 v3.4.0 // indirect
+	github.com/lucasb-eyer/go-colorful v1.4.0 // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
+	golang.org/x/sys v0.44.0 // indirect
+	golang.org/x/term v0.43.0 // indirect
+	golang.org/x/text v0.37.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,136 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/gdamore/tcell/v2"
+)
+
+const paddleSymbol = 0x2588
+const paddleHeight = 4
+
+type Paddle struct {
+	row, col, width, height int
+}
+
+var screen tcell.Screen
+var player1 *Paddle
+var player2 *Paddle
+var debugLog string
+
+func main() {
+
+	initScreen()
+	InitGameState()
+
+	inputChan := InitUserInput()
+
+	for {
+		drawState()
+		time.Sleep(50 * time.Millisecond)
+
+		key := ReadInput(inputChan)
+		HandleUserInput(key)
+
+	}
+}
+
+func drawState() {
+	screen.Clear()
+	screen.SetTitle("Ping Pong")
+	PrintString(0, 0, debugLog)
+
+	Print(player1.row, player1.col, player1.width, player1.height, paddleSymbol)
+	Print(player2.row, player2.col, player2.width, player2.height, paddleSymbol)
+
+	screen.Show()
+}
+
+func initScreen() {
+	var err error
+	screen, err = tcell.NewScreen()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", err)
+		os.Exit(1)
+	}
+
+	if err := screen.Init(); err != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", err)
+		os.Exit(1)
+	}
+
+	defStyle := tcell.StyleDefault.
+		Background(tcell.ColorBlack).
+		Foreground(tcell.ColorWhite)
+	screen.SetStyle(defStyle)
+}
+
+func HandleUserInput(key string) {
+	_, screenHeight := screen.Size()
+	if key == "Rune[q]" {
+		screen.Fini()
+		os.Exit(0)
+	} else if key == "Rune[w]" && player1.row > 0 {
+		player1.row--
+	} else if key == "Rune[s]" && player1.row+player1.height < screenHeight {
+		player1.row++
+	} else if key == "Up" && player2.row > 0 {
+		player2.row--
+	} else if key == "Down" && player2.row+player2.height < screenHeight {
+		player2.row++
+	}
+}
+
+func InitUserInput() chan string {
+	inputChan := make(chan string)
+	go func() {
+		for {
+			switch ev := screen.PollEvent().(type) {
+			case *tcell.EventKey:
+				inputChan <- ev.Name()
+			}
+		}
+	}()
+	return inputChan
+}
+
+func InitGameState() {
+	width, height := screen.Size()
+	paddleStart := height/2 - paddleHeight/2
+
+	player1 = &Paddle{
+		row: paddleStart, col: 0, width: 1, height: paddleHeight,
+	}
+
+	player2 = &Paddle{
+		row: paddleStart, col: width - 1, width: 1, height: paddleHeight,
+	}
+
+}
+
+func ReadInput(inputChan chan string) string {
+	var key string
+	select {
+	case key = <-inputChan:
+	default:
+		key = ""
+	}
+	return key
+}
+
+func PrintString(row, col int, str string) {
+	for _, c := range str {
+		screen.SetContent(col, row, c, nil, tcell.StyleDefault)
+		col += 1
+	}
+}
+
+func Print(row, col, width, height int, ch rune) {
+	for r := 0; r < height; r++ {
+		for c := 0; c < width; c++ {
+			screen.SetContent(col+c, row+r, ch, nil, tcell.StyleDefault)
+		}
+	}
+}
